22884b19aa2daf9d8bfb13932fc15ec6a8e3041a
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-06-22 21:52:43 -0500

    Pushing to Supabase
     - Set up for every 5 seconds interval in the background to begin doing this

--- timeTracker/App.tsx
+++ timeTracker/App.tsx
@@ -2,11 +2,19 @@ import { NewAppScreen } from '@react-native/new-app-screen';
 import { useEffect } from 'react';
 import { NativeModules, StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import BackgroundJob from 'react-native-background-actions';
+// Needed for supabase
+import 'react-native-url-polyfill/auto';
+import { createClient } from '@supabase/supabase-js';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { DB_PUBLISHABLE_KEY } from '@env';
 const { UsageStatsModule } = NativeModules;
+
+
+const supabase = createClient('https://dhyrzbqugxgtjaurnczc.supabase.co', DB_PUBLISHABLE_KEY);
+
 
 const options = {
     taskName: 'Example',
@@ -24,22 +32,33 @@ const options = {
 };
 
 const veryIntensiveTask = async (taskDataArguments: any) => {
-    console.log("Starting!");
-    await new Promise(async (resolve) => {
-      console.log("finished!");
-      console.log(BackgroundJob.isRunning(), taskDataArguments)
-  });
+  const { delay } = taskDataArguments;
+  console.log(BackgroundJob.isRunning(), delay)
+
+  setInterval(() => {
+    console.log("This message appears after 5 seconds.");
+  }, 5000);
 }
 
 const fetchUsageData = async () => {
-  const hasPermission = await UsageStatsModule.checkPermission();
-  if (!hasPermission) {
-    UsageStatsModule.openSettings();
-    return;
+  // const hasPermission = await UsageStatsModule.checkPermission();
+  // if (!hasPermission) {
+  //   UsageStatsModule.openSettings();
+  //   return;
+  // }
+  const currentApplication = {
+      device: "test",
+      event_time: new Date(),
+      application_name: "test",
+      state: "active",
+      app_type: "test"
   }
-
-  const packageName = await UsageStatsModule.getFocusedApp();
-  return packageName;
+  // const packageName = await UsageStatsModule.getFocusedApp();
+  // return packageName;
+  supabase.from('time_events').insert(currentApplication).select().then((data) => {
+      console.log("data: ", data);
+      console.log("error: ", data.error);
+  })
 };
 
 function App() {
