1137ef62e48a87017d1504f96449f10ef1d54275
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-07-02 22:32:24 -0500

    Basics of Chart.js set up
     - Very finnicky to set up
     - TODO fix the errors on security access and redefinition of the variable in the renderer

--- desktop-app/renderer.js
+++ desktop-app/renderer.js
@@ -3,6 +3,7 @@ const databaseText = document.getElementById('databaseOutputText')
 const backButton = document.getElementById('dateBackBtn')
 const forwardButton = document.getElementById('dateForwardBtn')
 const currentDayText = document.getElementById('currentDayText')
+const chartElement = document.getElementById('chart')
 
 setButton.addEventListener('click', () => {
   window.electronAPI.pushToDatabase();
@@ -32,6 +33,38 @@ window.addEventListener('beforeunload', (event) => {
   // window.electronAPI.exampleFunction();
 });
 
+let chart;
+
+const createChart = () => {  
+  if (chart) {
+    myChart.destroy();
+  }
+  const xyValues = [
+  {x:50, y:7},
+  {x:60, y:8},
+  {x:70, y:8},
+  {x:80, y:9},
+  {x:90, y:9},
+  {x:100, y:9},
+  {x:110, y:10},
+  {x:120, y:11},
+  {x:130, y:14},
+  {x:140, y:14},
+  {x:150, y:15}
+  ];
+
+  chart = new Chart(chartElement, {
+  type: "scatter",
+  data: {
+      datasets: [{
+      pointRadius: 4,
+      pointBackgroundColor: "rgba(0,0,255,1)",
+      data: xyValues
+      }]
+  },
+  options: {}
+  });
+}
 
 async function loadContent() {
   // Update the tag to connect renderer with main process
@@ -56,6 +89,8 @@ async function loadContent() {
   databaseText.innerText = cleanedOutput;
   
   currentDayText.innerText = await window.electronAPI.grabCurrentDate();
+
+  createChart();
 };
 
 
